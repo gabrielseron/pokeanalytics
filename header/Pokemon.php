697496b3cfd3c88be05056a1155004ccc1042c1b
Azenstrange <edouard.eisenfisz@epita.fr>
2020-06-22 18:07:09 +0200

    :sparkles: header doc

---
+++ header/Pokemon.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <title>Poké Pro Strat</title>
+  <meta name="description" content="Description de ma page pour les moteurs de recherche">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link rel="stylesheet" href="./styles/main.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.5/fullpage.min.css" />
+</head>
+<body class="container">
+  <div class="">
+  <div id="nav">
+  <nav>
+    <div class="cubenav"></div>
+    <div class="cubenavOne"></div>
+    <div class="cubenavTwo">
+      <img src="./images/togekiss1.png" alt="togekiss">
+    </div>
+    <div class="cubenavThree"></div>
+    <div class="cubenavFour"></div>
+    <div class="cubenavFive"></div>
+    <div class="cubenavSix"></div>
+    <div class="cubenavSeven"></div>
+    <div class="rightCubenav">
+      <img src="./images/togekiss2.png" alt="togekiss">
+    </div>
+    <div class="rightCubenavOne"></div>
+    <div class="rightCubenavTwo"></div>
+    <div class="rightCubenavThree"></div>
+    <div class="rightCubenavFour"></div>
+    <div class="rightCubenavFive"></div>
+    <div class="rightCubenavSix"></div>
+    <div class="rightCubenavSeven"></div>
+    <div class="titlemove">
+      <ul class="menutitle titleuber">
+        <li class="titlevalue"><h2>Togekiss</h2></li>
+        <li class="titlevalue"><h2>UBER</h2></li>
+      </ul>
+      <ul class="type">
+        <li class="valuetiny"><p>Fairy</p></li>
+        <li class="valuetiny tinyone"><p>Flying</p></li>
+        <li class="valuetiny"><p>N°1</p></li>
+        <li class="valuetiny"><p>41.22%</p></li>
+      </ul>
+    </div>
+    <div class="move">
+      <ul class="menu open translatone">
+        <li class="ribbon"><img src="images/Ribbon3.png" class="ribbon" alt=""></li>
+        <li class="value"><p>90%</p></li>
+      </ul>
+      <ul class="menu open translatsec">
+        <li class="ribbon"><img src="images/Ribbon2.png" class="ribbon" alt=""></li>
+        <li class="value"><p>40%</p></li>
+      </ul> 
+      <ul class="menu open">
+        <li class="ribbon"><img src="images/Ribbon1.png" class="ribbon" alt=""></li>
+        <li class="value"><p>10%</p></li>
+      </ul>
+    </div>
+
+  </nav>
+</div>
+  <div id="footer">
+  <footer class="footer" >
+    <hr>
+    <img src="images/logo_rempli.svg" class="footer_logo" alt="">
+    <div class="footer_brand">Pokémon Pro Stats</div>
+  </footer>
+  </div>
+<div id="fullPage">
+  <div class="container lou, section">
+    <img src="images/lou1.jpg" alt="">
+    <img src="images/lou2.jpg" alt="">
+  </div>
+  <div class="container yacine, section">
+    <img src="images/yacine2.jpg" alt="">
+    <img src="images/yacine1.png" alt="">
+  </div>
+  <div class="container affiches, section">
+    <img src="images/affiche1.png" alt="">
+    <img src="images/affiche2.png" alt="">
+  </div>
+</div>
+<script src="scripts/main.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.5/fullpage.min.js"></script>"
+
+  <script>
+    new fullpage('#fullPage', {
+      autoScrolling: true,
+      navigation: true,
+      navigationTooltips: ["Test", "Test2", "Test3"],
+      showActiveTooltip: true
+    })
+  </script>
+
+</body>
+</html
