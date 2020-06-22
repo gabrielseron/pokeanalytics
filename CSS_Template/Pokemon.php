1b4e62146ca4dcb7ed199518d48df476861c39ab
Rii- <bibohenri@hotmail.fr>
2020-06-22 15:03:59 +0200

    Added responsive moving background
    Centered navbar

--- CSS_Template/Pokemon.php
+++ CSS_Template/Pokemon.php
@@ -16,33 +16,33 @@
       <li class="search"><a href="gallerie.html">Search</a></li>
     </ul>
   </nav>
-</div>
-  <div id="footer">
-  <footer class="footer" >
-    <hr>
-    <img src="images/logo_rempli.svg" class="footer_logo" alt="">
-    <div class="footer_brand">Pokémon Pro Stats</div>
-    <span class="footer_contact">
-      <a href="mailto:contac@safetybear.co">Contact Us</a>
-    </span>
-  </footer>
   </div>
-<div id="fullPage">
-  <div class="container lou, section">
-    <img src="images/lou1.jpg" alt="">
-    <img src="images/lou2.jpg" alt="">
+    <div id="footer">
+    <footer class="footer" >
+      <hr>
+      <img src="images/logo_rempli.svg" class="footer_logo" alt="">
+      <div class="footer_brand">Pokémon Pro Stats</div>
+      <span class="footer_contact">
+        <a href="mailto:contac@safetybear.co">Contact Us</a>
+      </span>
+    </footer>
+    </div>
+  <div id="fullPage">
+    <div class="container lou, section">
+      <img src="images/lou1.jpg" alt="">
+      <img src="images/lou2.jpg" alt="">
+    </div>
+    <div class="container yacine, section">
+      <img src="images/yacine2.jpg" alt="">
+      <img src="images/yacine1.png" alt="">
+    </div>
+    <div class="container affiches, section">
+      <img src="images/affiche1.png" alt="">
+      <img src="images/affiche2.png" alt="">
+    </div>
   </div>
-  <div class="container yacine, section">
-    <img src="images/yacine2.jpg" alt="">
-    <img src="images/yacine1.png" alt="">
-  </div>
-  <div class="container affiches, section">
-    <img src="images/affiche1.png" alt="">
-    <img src="images/affiche2.png" alt="">
-  </div>
-</div>
-<script src="scripts/main.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.5/fullpage.min.js"></script>"
+  <script src="scripts/main.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.5/fullpage.min.js"></script>"
 
   <script>
     new fullpage('#fullPage', {
