8240abc76ddf49d2b379d92b817032b3aee00a9a
Azenstrange <edouard.eisenfisz@epita.fr>
2020-06-23 14:37:55 +0200

    :sparkles: menu

--- header/Pokemon.php
+++ header/Pokemon.php
@@ -9,21 +9,9 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.5/fullpage.min.css" />
 </head>
 <body class="container">
-  <div class="">
-  <div id="nav">
-  <nav>
-    <div class="cubenav"></div>
-    <div class="cubenavOne"></div>
-    <div class="cubenavTwo">
-      <img src="./images/togekiss1.png" alt="togekiss">
-    </div>
-    <div class="cubenavThree"></div>
-    <div class="cubenavFour"></div>
-    <div class="cubenavFive"></div>
-    <div class="cubenavSix"></div>
-    <div class="cubenavSeven"></div>
+  <div class="topfix">
     <div class="rightCubenav">
-      <img src="./images/togekiss2.png" alt="togekiss">
+        <img src="./images/togekiss2.png" alt="togekiss">
     </div>
     <div class="rightCubenavOne"></div>
     <div class="rightCubenavTwo"></div>
@@ -32,68 +20,74 @@
     <div class="rightCubenavFive"></div>
     <div class="rightCubenavSix"></div>
     <div class="rightCubenavSeven"></div>
-    <div class="titlemove">
-      <ul class="menutitle titleuber">
-        <li class="titlevalue"><h2>Togekiss</h2></li>
-        <li class="titlevalue"><h2>UBER</h2></li>
-      </ul>
-      <ul class="type">
-        <li class="valuetiny"><p>Fairy</p></li>
-        <li class="valuetiny tinyone"><p>Flying</p></li>
-        <li class="valuetiny"><p>N°1</p></li>
-        <li class="valuetiny"><p>41.22%</p></li>
-      </ul>
     </div>
-    <div class="move">
-      <ul class="menu open translatone">
-        <li class="ribbon"><img src="images/Ribbon3.png" class="ribbon" alt=""></li>
-        <li class="value"><p>90%</p></li>
-      </ul>
-      <ul class="menu open translatsec">
-        <li class="ribbon"><img src="images/Ribbon2.png" class="ribbon" alt=""></li>
-        <li class="value"><p>40%</p></li>
-      </ul> 
-      <ul class="menu open">
-        <li class="ribbon"><img src="images/Ribbon1.png" class="ribbon" alt=""></li>
-        <li class="value"><p>10%</p></li>
-      </ul>
-    </div>
-  </nav>
-  <main>
-    <div class="baseStats">
-      <div class="baseStatsTitle">
-        <h2>Base stats</h2>
-      </div>
-    </div>
-    <div class="movesChoices">
-      <div class="movesTitle">
-        <h2>Moves</h2>
-      </div>
-    </div>
-  </main>
- 
 </div>
-  <div id="footer">
-  <footer class="footer" >
-    <hr>
-    <img src="images/logo_rempli.svg" class="footer_logo" alt="">
-    <div class="footer_brand">Pokémon Pro Stats</div>
-  </footer>
-  </div>
 <div id="fullPage">
   <div class="container lou, section">
-    <img src="images/lou1.jpg" alt="">
-    <img src="images/lou2.jpg" alt="">
+    <div id="nav">
+      <nav>
+        <div class="cubenav"></div>
+        <div class="cubenavOne"></div>
+        <div class="cubenavTwo">
+          <img src="./images/togekiss1.png" alt="togekiss">
+        </div>
+        <div class="cubenavThree"></div>
+        <div class="cubenavFour"></div>
+        <div class="cubenavFive"></div>
+        <div class="cubenavSix"></div>
+        <div class="cubenavSeven"></div>
+        <div class="titlemove">
+          <ul class="menutitle titleuber">
+            <li class="titlevalue"><h2>Togekiss</h2></li>
+            <li class="titlevalue"><h2>UBER</h2></li>
+          </ul>
+          <ul class="type">
+            <li class="valuetiny"><p>Fairy</p></li>
+            <li class="valuetiny tinyone"><p>Flying</p></li>
+            <li class="valuetiny"><p>N°1</p></li>
+            <li class="valuetiny"><p>41.22%</p></li>
+          </ul>
+        </div>
+        <div class="move">
+          <div class="ribbonone">
+            <ul class="menu open translatone">
+              <li class="ribbon"><img src="images/Ribbon3.png" class="ribbon" alt=""></li>
+              <li class="value"><p>90%</p></li>
+            </ul>
+          </div>
+          <div class="ribbontwo">
+            <ul class="menu open translatsec">
+              <li class="ribbon"><img src="images/Ribbon2.png" class="ribbon" alt=""></li>
+              <li class="value"><p>40%</p></li>
+            </ul> 
+          </div>
+          <div class="ribbonthree">
+            <ul class="menu open">
+              <li class="ribbon"><img src="images/Ribbon1.png" class="ribbon" alt=""></li>
+              <li class="value"><p>10%</p></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   </div>
   <div class="container yacine, section">
-    <img src="images/yacine2.jpg" alt="">
-    <img src="images/yacine1.png" alt="">
+    <div class="botnav">  
+      <div class="cubebotnav"></div>
+      <div class="cubebotnavOne"></div>
+      <div class="cubebotnavTwo">
+        <img src="./images/togekiss1.png" alt="togekiss">
+      </div>
+      <div class="cubebotnavThree"></div>
+      <div class="cubebotnavFive"></div>
+      <div class="cubebotnavSix"></div>
+      <div class="cubebotnavSeven"></div>
+    </div>
   </div>
   <div class="container affiches, section">
-    <img src="images/affiche1.png" alt="">
-    <img src="images/affiche2.png" alt="">
+  
   </div>
-</div>
+</div>  
 <script src="scripts/main.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.5/fullpage.min.js"></script>"
 
